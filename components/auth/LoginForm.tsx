c4d334b6bf43d6a67972e22d905b1ef22e591a33
phizy.ts <email@phizy.dev>
2025-07-15 21:28:39 -0400

    fix: nested Form component in login page

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -45,79 +45,78 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <Form
-            action={async (formData) => {
-              startTransition(async () => {
-                await signInWithEmail(formData, setIsOTPOpen);
-              });
-            }}
-          >
-            <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Form action={signInWithGithub}>
-                  <Button variant="outline" className="w-full" type="submit">
-                    <img src="/github_light.svg" alt="github" />
-                    Login with GitHub
-                  </Button>
-                </Form>
-                <Form action={signInWithGoogle}>
-                  <Button variant="outline" className="w-full" type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    Login with Google
-                  </Button>
-                </Form>
-              </div>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-              <div className="grid gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    name="email"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="/auth/forgot-password"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    placeholder="••••••••"
-                    required
-                  />
-                </div>
-                <Button type="submit" disabled={isPending} className="w-full">
-                  {isPending ? <Loading isWhite /> : "Sign in"}
+          <div className="grid gap-6">
+            <div className="flex flex-col gap-4">
+              <Form action={signInWithGithub}>
+                <Button variant="outline" className="w-full" type="submit">
+                  <img src="/github_light.svg" alt="github" />
+                  Login with GitHub
                 </Button>
-              </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="/auth/signup" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
+              </Form>
+              <Form action={signInWithGoogle}>
+                <Button variant="outline" className="w-full" type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  Login with Google
+                </Button>
+              </Form>
             </div>
-          </Form>
+            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+            <Form
+              action={async (formData) => {
+                startTransition(async () => {
+                  await signInWithEmail(formData, setIsOTPOpen);
+                });
+              }}
+              className="grid gap-6"
+            >
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  name="email"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="/auth/forgot-password"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
+              <Button type="submit" disabled={isPending} className="w-full">
+                {isPending ? <Loading isWhite /> : "Sign in"}
+              </Button>
+            </Form>
+            <div className="text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/auth/signup" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </div>
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
